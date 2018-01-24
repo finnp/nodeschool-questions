2e7837fec26c3e51d8c2aa320f4dd0b4b9222040
Finn Pauls <derfinn@gmail.com>
2018-01-24 02:39:11 +0100

    chore: add standard

--- level.js
+++ level.js
@@ -1,7 +1,8 @@
 var level = require('level')
 var sublevel = require('level-spaces')
+var path = require('path')
 
-var db = level(__dirname + '/db')
+var db = level(path.join(__dirname, '/db'))
 
 module.exports = function (space) {
   return sublevel(db, space)
